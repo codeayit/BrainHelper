bfc05e211b3b3123b515099973de8cfd74fbbd07
1053452188@qq.com <lny199211>
2019-02-14 15:16:04 +0800

    增加tts 以及回调

--- app/src/main/java/com/robot/brainhelper/MainActivity.java
+++ app/src/main/java/com/robot/brainhelper/MainActivity.java
@@ -2,13 +2,40 @@ package com.robot.brainhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.robot.brainhelperlib.BrainHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private BrainHelper brainHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        brainHelper = new BrainHelper(this);
+        brainHelper.regist();
 
+        brainHelper.setOnTtsListener(new BrainHelper.OnTtsListener() {
+            @Override
+            public void onStart() {
+                Log.d("klog","tts_start");
+            }
+
+            @Override
+            public void onStop() {
+                Log.d("klog","tts_stop");
+
+            }
+        });
+    }
+
+
+    public void tts(View view){
+        brainHelper.startTts("大脑助手");
     }
 }
--- brainhelperlib/src/main/java/com/robot/brainhelperlib/BrainHelper.java
+++ brainhelperlib/src/main/java/com/robot/brainhelperlib/BrainHelper.java
@@ -4,9 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.text.TextUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -53,6 +55,7 @@ public class BrainHelper {
         }
 
         actions.add("com.robot.brain.end");
+        actions.add("tts_stop");
 
         if (actions.size() > 0) {
             IntentFilter filter = new IntentFilter();
@@ -62,13 +65,18 @@ public class BrainHelper {
             mContext.registerReceiver(receiver = new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context context, Intent intent) {
-                    if (onActionListener != null) {
                         if ("com.robot.brain.end".equals(intent.getAction())) {
-                            onActionListener.onFinish(intent);
-                        } else {
+                            if (onActionListener != null) {
+                                onActionListener.onFinish(intent);}
+                        } else if("tts_stop".equals(intent.getAction())){
+                            if (!TextUtils.isEmpty(ttsToken) && ttsToken.equals(intent.getStringExtra("tts_token"))){
+                                if (onTtsListener!=null){
+                                    onTtsListener.onStop();
+                                }
+                            }
+                        }else {
                             onActionListener.onAction(intent);
                         }
-                    }
 
 
 //                    for (OnActionListener listener:listeners){
@@ -103,14 +111,44 @@ public class BrainHelper {
         mContext.sendBroadcast(intent);
     }
 
+//    public void sendText(String action, Map<String, String> data) {
+//        Intent intent = new Intent(action);
+//        if (data != null)
+//            for (Map.Entry<String, String> entry : data.entrySet()) {
+//                intent.putExtra(entry.getKey(), entry.getValue());
+//            }
+//        mContext.sendBroadcast(intent);
+//    }
+
     public void sendText(String action, Map<String, String> data) {
-        Intent intent = new Intent(action);
+        Intent intent = new Intent("com.robot.brain");
+        intent.putExtra("data",action);
         if (data != null)
             for (Map.Entry<String, String> entry : data.entrySet()) {
                 intent.putExtra(entry.getKey(), entry.getValue());
             }
         mContext.sendBroadcast(intent);
     }
+
+    private String ttsToken;
+
+    public void startTts(String text){
+        ttsToken = String.valueOf(System.currentTimeMillis());
+        Map<String,String> data = new HashMap<>();
+        data.put("tts_text",text);
+        data.put("tts_token",ttsToken);
+        sendText("tts_start",data);
+        if (onTtsListener!=null){
+            onTtsListener.onStart();
+        }
+    }
+
+    public void stopTts(){
+        send("tts_stop");
+    }
+
+
+
 
     /**
      * 释放mic
@@ -157,6 +195,8 @@ public class BrainHelper {
         send("nextQ");
     }
 
+
+
     /**
      * 结束队列
      */
@@ -169,6 +209,18 @@ public class BrainHelper {
         void onAction(Intent intent);
 
         void onFinish(Intent intent);
+    }
+
+
+    public void setOnTtsListener(OnTtsListener onTtsListener) {
+        this.onTtsListener = onTtsListener;
+    }
+
+    private OnTtsListener onTtsListener;
+
+    public interface OnTtsListener{
+        void onStart();
+        void onStop();
     }
 
 
